17bcbef3d2093ec1544233b6d3d33f4213436eb0
Andrew Kondratev <andruhon@gmail.com>
2019-07-17 00:26:04 +1200

    WICKET-6682 code-review changes; pt. 4

--- wicket-util/src/main/java/org/apache/wicket/util/value/ValueMap.java
+++ wicket-util/src/main/java/org/apache/wicket/util/value/ValueMap.java
@@ -23,6 +23,7 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.apache.wicket.util.encoding.UrlEncoder;
 import org.apache.wicket.util.parse.metapattern.MetaPattern;
 import org.apache.wicket.util.parse.metapattern.parsers.VariableAssignmentParser;
 import org.apache.wicket.util.string.IStringIterator;
@@ -63,9 +64,6 @@ public class ValueMap extends LinkedHashMap<String, Object> implements IValueMap
 	/** an empty <code>ValueMap</code>. */
 	public static final ValueMap EMPTY_MAP;
 
-	/** keys values of which not to be escaped */
-	private final Collection<String> noEscapeMarkupKeys = new HashSet<>();
-
 	/** create EMPTY_MAP, make immutable * */
 	static
 	{
@@ -86,20 +84,6 @@ public class ValueMap extends LinkedHashMap<String, Object> implements IValueMap
 	public ValueMap()
 	{
 		super();
-	}
-
-	/**
-	 * Constructs empty <code>ValueMap</code>.
-	 * @param noEscapeMarkupKeys
-	 * 		a set of keys to not aply
-	 */
-	public ValueMap(Collection<String> noEscapeMarkupKeys)
-	{
-		super();
-		if (noEscapeMarkupKeys != null)
-		{
-			this.noEscapeMarkupKeys.addAll(noEscapeMarkupKeys);
-		}
 	}
 
 	/**
@@ -188,13 +172,6 @@ public class ValueMap extends LinkedHashMap<String, Object> implements IValueMap
 				equalsIndex = -1;
 			}
 		}
-	}
-
-	/**
-	 * Add a key which should not be escaped when map is rendered wit {@link #toString()}
-	 */
-	public void addNoEscapeKey(String key) {
-		noEscapeMarkupKeys.add(key);
 	}
 
 	/**
@@ -591,11 +568,7 @@ public class ValueMap extends LinkedHashMap<String, Object> implements IValueMap
 			}
 			else
 			{
-				buffer.append(
-						!noEscapeMarkupKeys.isEmpty() && noEscapeMarkupKeys.contains(key)
-								? value
-								: Strings.escapeMarkup(String.valueOf(value))
-				);
+				buffer.append(UrlEncoder.FULL_URL_INSTANCE.encode(String.valueOf(value), "UTF-8"));
 			}
 
 			buffer.append('\"');
